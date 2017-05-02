a1bc9f9fef5e7aac666763e34ab9d46fd7becd81
tn_mai <rabitfiver@gmail.com>
2017-05-02 09:44:14 +0900

    インデックスバッファを使用した描画を実装.

--- 03/OpenGLTutorial03/Src/Main.cpp
+++ 03/OpenGLTutorial03/Src/Main.cpp
@@ -27,12 +27,22 @@ struct Vertex
 /// 頂点データ.
 const Vertex vertices[] = {
   { {-0.5f,  0.5f, 0.5f}, {1.0f, 0.0f, 0.0f, 1.0f} },
-  { { 0.5f, -0.5f, 0.5f}, {0.0f, 1.0f, 0.0f, 1.0f} },
-  { {-0.5f, -0.5f, 0.5f}, {0.0f, 0.0f, 1.0f, 1.0f} },
+  { { 0.3f,  0.5f, 0.5f}, {0.0f, 0.0f, 1.0f, 1.0f} },
+  { { 0.3f, -0.3f, 0.5f}, {0.0f, 1.0f, 0.0f, 1.0f} },
+  { {-0.5f, -0.3f, 0.5f}, {0.0f, 0.0f, 1.0f, 1.0f} },
 
-  { {-0.5f,  0.5f, 0.5f}, {1.0f, 0.0f, 0.0f, 1.0f} },
-  { { 0.5f,  0.5f, 0.5f}, {0.0f, 0.0f, 1.0f, 1.0f} },
-  { { 0.5f, -0.5f, 0.5f}, {0.0f, 1.0f, 0.0f, 1.0f} },
+  { {-0.3f,  0.3f, 0.6f}, {1.0f, 0.0f, 0.0f, 1.0f} },
+  { { 0.5f,  0.3f, 0.6f}, {1.0f, 1.0f, 0.0f, 1.0f} },
+  { { 0.5f, -0.5f, 0.6f}, {1.0f, 0.0f, 0.0f, 1.0f} },
+  { { 0.5f, -0.5f, 0.6f}, {0.0f, 0.0f, 1.0f, 1.0f} },
+  { {-0.3f, -0.5f, 0.6f}, {0.0f, 1.0f, 1.0f, 1.0f} },
+  { {-0.3f,  0.3f, 0.6f}, {0.0f, 0.0f, 1.0f, 1.0f} },
+};
+
+/// インデックスデータ.
+const GLuint indices[] = {
+  0, 1, 2, 2, 3, 0,
+  4, 5, 6, 7, 8, 9,
 };
 
 /// 頂点シェーダ.
@@ -74,6 +84,24 @@ GLuint CreateVBO(GLsizeiptr size, const GLvoid* data)
 }
 
 /**
+* Index Buffer Objectを作成する.
+*
+* @param size インデックスデータのサイズ.
+* @param data インデックスデータへのポインタ.
+*
+* @return 作成したIBO.
+*/
+GLuint CreateIBO(GLsizeiptr size, const GLvoid* data)
+{
+  GLuint ibo = 0;
+  glGenBuffers(1, &ibo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+  return ibo;
+}
+
+/**
 * 頂点アトリビュートを設定する.
 *
 * @param index 頂点アトリビュートのインデックス.
@@ -91,11 +119,12 @@ void SetVertexAttribPointerI(GLuint index, GLint size, GLsizei stride, const GLv
 /**
 * Vertex Array Objectを作成する.
 *
-* @param vbo VAOに関連付けられるVBOのID.
+* @param vbo VAOに関連付けられるVBO.
+* @param ibo VAOに関連付けられるIBO.
 *
-* @return 作成したVAOのID.
+* @return 作成したVAO.
 */
-GLuint CreateVAO(GLuint vbo)
+GLuint CreateVAO(GLuint vbo, GLuint ibo)
 {
   GLuint vao = 0;
   glGenVertexArrays(1, &vao);
@@ -103,6 +132,7 @@ GLuint CreateVAO(GLuint vbo)
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   SetVertexAttribPointer(0, Vertex, position);
   SetVertexAttribPointer(1, Vertex, color);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
   glBindVertexArray(0);
   return vao;
 }
@@ -188,11 +218,14 @@ int main()
   }
 
   const GLuint vbo = CreateVBO(sizeof(vertices), vertices);
-  const GLuint vao = CreateVAO(vbo);
+  const GLuint ibo = CreateIBO(sizeof(indices), indices);
+  const GLuint vao = CreateVAO(vbo, ibo);
   const GLuint shaderProgram = CreateShaderProgram(vsCode, fsCode);
-  if (!vbo || !vao || !shaderProgram) {
+  if (!vbo || !ibo || !vao || !shaderProgram) {
 	  return 1;
   }
+
+//  glEnable(GL_DEPTH_TEST);
 
   while (!window.ShouldClose()) {
     glClearColor(0.1f, 0.3f, 0.5f, 1.0f);
@@ -200,7 +233,7 @@ int main()
 
     glUseProgram(shaderProgram);
     glBindVertexArray(vao);
-    glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices)/sizeof((vertices)[0]));
+    glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(indices[0]), GL_UNSIGNED_INT, reinterpret_cast<const GLvoid*>(0));
 
     window.SwapBuffers();
   }
