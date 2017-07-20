a6d53b39f093163cec353cc439353d31fb20e756
tn_mai <rabitfiver@gmail.com>
2017-07-20 20:01:18 +0900

    タイトル画面を作成.

--- 12/OpenGLTutorial12/Src/Main.cpp
+++ 12/OpenGLTutorial12/Src/Main.cpp
@@ -308,6 +308,41 @@ struct Update
   Entity::Entity* pSpaceSphere = nullptr;
 };
 
+/**
+* タイトル画面.
+*/
+struct TitleState
+{
+  void operator()(double delta)
+  {
+    GameEngine& game = GameEngine::Instance();
+    game.Camera({ glm::vec4(0, 20, -8, 1), glm::vec3(0, 0, 12), glm::vec3(0, 0, 1) });
+
+    if (!pSpaceSphere) {
+      pSpaceSphere =  game.AddEntity(EntityGroupId_Others, glm::vec3(0, 0, 0), "SpaceSphere", "Res/Model/SpaceSphere.bmp", nullptr, false);
+    }
+    glm::vec3 rotSpace = glm::eulerAngles(pSpaceSphere->Rotation());
+    rotSpace.x += static_cast<float>(glm::radians(2.5) * delta);
+    pSpaceSphere->Rotation(rotSpace);
+
+    game.FontPropotional(true);
+    game.FontThickness(0.25f);
+    game.FontBorder(0.25f);
+    game.FontColor({ 0.9f, 0.95f, 1.0f, 1.0f });
+    game.FontSubColor({ 0.1f, 0.5f, 0.8f, 0.8f });
+    game.FontScale(glm::vec2(2));
+    game.AddString(glm::vec2(-0.25f, 0.125f), "star fighter");
+    game.FontScale(glm::vec2(0.5f));
+    game.FontBorder(0.0f);
+    game.AddString(glm::vec2(0.1f, -0.05f), "the ultimate in manned-fighter");
+    if (game.GetGamePad(0).buttonDown & (GamePad::A | GamePad::B | GamePad::START)) {
+      game.RemoveEntity(pSpaceSphere);
+      game.UpdateFunc(Update());
+    }
+  }
+  Entity::Entity* pSpaceSphere = nullptr;
+};
+
 /// エントリーポイント.
 int main()
 {
@@ -326,7 +361,7 @@ int main()
   game.LoadFontFromFile("Res/Font.fnt");
 
   game.CollisionHandler(EntityGroupId_PlayerShot, EntityGroupId_Enemy, &CollidePlayerShotAndEnemyHandler);
-  game.UpdateFunc(Update());
+  game.UpdateFunc(TitleState());
   game.Run();
 
   return 0;
