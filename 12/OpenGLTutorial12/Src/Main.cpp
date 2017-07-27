6366e3b3057306a7c4226bbd4dde562e57306937
tn_mai <rabitfiver@gmail.com>
2017-07-27 09:12:18 +0900

    背景のモデルデータをリピート不要なものに変更したので消す.

--- 12/OpenGLTutorial12/Src/Main.cpp
+++ 12/OpenGLTutorial12/Src/Main.cpp
@@ -18,7 +18,7 @@ int main()
 
   game.LoadTextureFromFile("Res/Model/Toroid.bmp");
   game.LoadTextureFromFile("Res/Model/Player.bmp");
-  game.LoadTextureFromFile("Res/Model/SpaceSphere.bmp", GL_REPEAT);
+  game.LoadTextureFromFile("Res/Model/SpaceSphere.bmp");
   game.LoadMeshFromFile("Res/Model/Player.fbx");
   game.LoadMeshFromFile("Res/Model/Toroid.fbx");
   game.LoadMeshFromFile("Res/Model/Blast.fbx");
