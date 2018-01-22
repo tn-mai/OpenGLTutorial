58f7a0bb9c2b3ee3c1c1c2a86a9a6122ab9a94fc
tn_mai <rabitfiver@gmail.com>
2018-01-22 22:58:14 +0900

    ステージ背景の切り替えを実装.

--- 18/OpenGLTutorial18/Src/MainGameState.cpp
+++ 18/OpenGLTutorial18/Src/MainGameState.cpp
@@ -26,7 +26,7 @@ struct UpdateToroid
   {
     // 範囲外に出たら削除する.
     const glm::vec3 pos = entity.Position();
-    if (std::abs(pos.x) > 40.0f || std::abs(pos.z) > 40.0f) {
+    if (std::abs(pos.x) > 50.0f || std::abs(pos.z) > 50.0f) {
       GameEngine::Instance().RemoveEntity(&entity);
       return;
     }
@@ -229,7 +229,7 @@ static void UpdateSpaceSphere(Entity::Entity& entity, double delta)
 
 void UpdateLandscape(Entity::Entity& entity, double delta)
 {
-  entity.Position(entity.Position() + glm::vec3(0, 0, -2.0f * delta));
+  entity.Position(entity.Position() + glm::vec3(0, 0, -4.0f * delta));
 }
 /**
 *　コンストラクタ.
@@ -249,16 +249,19 @@ void MainGame::operator()(double delta)
 {
   GameEngine& game = GameEngine::Instance();
 
-  static const float stageTime = 120;
+  static const float stageTime = 60;
   static const float stageTitleTime = stageTime - 3;
+  static Entity::Entity* pBoss = nullptr;
 
   if (stageTimer < 0) {
+    //++stageNo;
     ++stageNo;
     stageTimer = stageTime;
 
     game.Camera({ glm::vec4(0, 20, -8, 1), glm::vec3(0, 0, 12), glm::vec3(0, 0, 1) });
     game.AmbientLight(glm::vec4(0.05f, 0.1f, 0.2f, 1));
     game.Light(0, { glm::vec4(40, 100, 10, 1), glm::vec4(12000, 12000, 12000, 1) } );
+//    game.Light(1, { glm::vec4(-40, -100, -10, 1), glm::vec4(120, 1200, 3000, 1) } );
     game.KeyValue(0.24f);
 
     game.RemoveAllEntity();
@@ -266,23 +269,28 @@ void MainGame::operator()(double delta)
 
     game.LoadTextureFromFile("Res/Model/Player.bmp");
     game.LoadMeshFromFile("Res/Model/Player.fbx");
+//    game.LoadMeshFromFile("Res/Model/Boss01.fbx");
 
     game.LoadMeshFromFile("Res/Model/Toroid.fbx");
     game.LoadMeshFromFile("Res/Model/Blast.fbx");
     game.LoadTextureFromFile("Res/Model/Toroid.bmp");
     game.LoadTextureFromFile("Res/Model/Toroid.Normal.bmp");
+//    game.LoadTextureFromFile("Res/Model/Boss01.Diffuse.bmp");
+//    game.LoadTextureFromFile("Res/Model/Boss01.Normal.bmp");
 
     switch (stageNo) {
     case 1: {
-      game.KeyValue(0.24f);
-      game.LoadMeshFromFile("Res/Model/BG01.fbx");
-      game.LoadTextureFromFile("Res/Model/BG01.Diffuse.bmp");
-      game.LoadTextureFromFile("Res/Model/BG01.Normal.bmp");
+      game.UserVariable(varPlayerStock) = 3;
+      game.KeyValue(0.16f);
+      game.LoadMeshFromFile("Res/Model/Landscape.fbx");
+//      game.LoadMeshFromFile("Res/Model/BG01.fbx");
+      game.LoadTextureFromFile("Res/Model/BG02.Diffuse.dds");
+      game.LoadTextureFromFile("Res/Model/BG02.Normal.bmp");
       game.LoadTextureFromFile("Res/Model/Block.Base.Diffuse.bmp");
       game.LoadTextureFromFile("Res/Model/Block.Base.Normal.bmp");
       game.LoadTextureFromFile("Res/Model/Block.End.Diffuse.bmp");
       game.LoadTextureFromFile("Res/Model/Block.End.Normal.bmp");
-      game.AddEntity(EntityGroupId_Others, glm::vec3(0, -60, 800), "Landscape01", "Res/Model/BG01.Diffuse.bmp", "Res/Model/BG01.Normal.bmp", &UpdateLandscape);
+
       for (int i = 0; i < 3; ++i) {
         auto p0 = game.AddEntity(EntityGroupId_Others, glm::vec3(3, -10, 30 + 50 * i), "Block.Base", "Res/Model/Block.Base.Diffuse.bmp", "Res/Model/Block.Base.Normal.bmp", UpdateLandscape);
         p0->Rotation(glm::vec3(0, 0, glm::radians(10.0f)));
@@ -293,16 +301,59 @@ void MainGame::operator()(double delta)
       p0->Rotation(glm::vec3(0, glm::radians(180.0f), glm::radians(190.0f)));
       auto p1 = game.AddEntity(EntityGroupId_Others, glm::vec3(-3, -10, 30 + 50 * 2), "Block.End", "Res/Model/Block.End.Diffuse.bmp", "Res/Model/Block.End.Normal.bmp", UpdateLandscape);
       p1->Rotation(glm::vec3(0, glm::radians(180.0f), glm::radians(-10.0f)));
+
+      for (int z = 0; z < 5; ++z) {
+        const float offsetZ = static_cast<float>(z * 40 * 5);
+        for (int x = 0; x < 5; ++x) {
+          const float offsetX = static_cast<float>(x * 40 - 80) * 5.0f;
+          auto entity = game.AddEntity(EntityGroupId_Others, glm::vec3(offsetX, -100, offsetZ), "Landscape01", "Res/Model/BG02.Diffuse.dds", "Res/Model/BG02.Normal.bmp", &UpdateLandscape);
+//          entity->Color(glm::vec4(2.5f, 2.5f, 2.5f, 1.0f));
+        }
+      }
+      
+      //      pBoss = game.AddEntity(EntityGroupId_Others, glm::vec3(0, -2, 30), "Boss01", "Res/Model/Boss01.Diffuse.bmp", "Res/Model/Boss01.Normal.bmp", nullptr);
+//      pBoss->Scale(glm::vec3(4));
+      break;
+    }
+    case 2: {
+      game.KeyValue(0.24f);
+      game.LoadMeshFromFile("Res/Model/City01.fbx");
+      game.LoadTextureFromFile("Res/Model/City01.Diffuse.dds");
+      game.LoadTextureFromFile("Res/Model/City01.Normal.bmp");
+      for (int z = 0; z < 12; ++z) {
+        const float offsetZ = static_cast<float>(z * 40);
+        for (int x = 0; x < 5; ++x) {
+          const float offsetX = static_cast<float>(x * 40 - 80);
+          auto entity = game.AddEntity(EntityGroupId_Others, glm::vec3(offsetX, -10, offsetZ), "City01", "Res/Model/City01.Diffuse.dds", "Res/Model/City01.Normal.bmp", &UpdateLandscape);
+          game.AddEntity(EntityGroupId_Others, glm::vec3(offsetX, -10, offsetZ), "City01.Shadow", "Res/Model/City01.Diffuse.dds", "Res/Model/City01.Normal.bmp", &UpdateLandscape);
+        }
+      }
       break;
     }
     default:
-    case 2: {
+    case 3: {
       game.KeyValue(0.02f);
       game.LoadMeshFromFile("Res/Model/SpaceSphere.fbx");
       game.LoadTextureFromFile("Res/Model/SpaceSphere.bmp");
       game.AddEntity(EntityGroupId_Others, glm::vec3(0, 0, 0), "SpaceSphere", "Res/Model/SpaceSphere.bmp", &UpdateSpaceSphere, "NonLighting");
       break;
     }
+#if 0
+    case 4: {
+      game.KeyValue(0.24f);
+      game.LoadMeshFromFile("Res/Model/Landscape.fbx");
+      game.LoadTextureFromFile("Res/Model/BG02.Diffuse.dds");
+      game.LoadTextureFromFile("Res/Model/BG02.Normal.bmp");
+      for (int z = 0; z < 5; ++z) {
+        const float offsetZ = static_cast<float>(z * 40 * 5);
+        for (int x = 0; x < 3; ++x) {
+          const float offsetX = static_cast<float>(x * 40 - 40) * 5.0f;
+          auto entity = game.AddEntity(EntityGroupId_Others, glm::vec3(offsetX, -60, offsetZ), "Landscape01", "Res/Model/BG02.Diffuse.dds", "Res/Model/BG02.Normal.bmp", &UpdateLandscape);
+        }
+      }
+      break;
+    }
+#endif
     }
 
     auto pPlayer = game.AddEntity(EntityGroupId_Player, glm::vec3(0, 0, 2), "Aircraft", "Res/Model/Player.bmp", UpdatePlayer());
@@ -317,16 +368,42 @@ void MainGame::operator()(double delta)
     game.FontScale(glm::vec2(2));
     game.FontColor(glm::vec4(1, 1, 1, alpha));
     game.FontSubColor(glm::vec4(0.1f, 0.5f, 0.8f, 0.8f * alpha));
-    game.AddString(glm::vec2(-0.25f, 0.125f), str);
+    game.AddString(glm::vec2(-0.3f, 0.125f), str);
   }
 
+  {
+    const int score = static_cast<int>(game.UserVariable(varScore));
+    char str[32];
+    snprintf(str, sizeof(str), "%08d", score);
+    game.FontScale(glm::vec2(1));
+    game.FontColor(glm::vec4(1, 1, 1, 1));
+    game.FontSubColor(glm::vec4(0.1f, 0.5f, 0.8f, 0.9f));
+    game.AddString(glm::vec2(-0.2f, 0.95f), str);
+  }
+
+
+  if (pBoss) {
+    const glm::quat rot = pBoss->Rotation();
+    float angle = glm::angle(rot);
+    angle += glm::radians(10.0f) * static_cast<float>(delta);
+    while (angle >= glm::radians(360.0f)) {
+      angle -= glm::radians(360.0f);
+    }
+    pBoss->Rotation(glm::angleAxis(angle, glm::vec3(0, 1, 0)));
+  }
+
+  GameEngine::CameraData camera = game.Camera();
+  float cameraMoveValue = fmod(static_cast<float>(stageTimer), 45.0f) * (glm::radians(360.0f) / 45.0f);
+  camera.position.x = glm::cos(cameraMoveValue) * 5.0f;
+  game.Camera(camera);
+
   std::uniform_int_distribution<> distributerX(-12, 12);
-  std::uniform_int_distribution<> distributerZ(40, 44);
+  std::uniform_int_distribution<> distributerZ(50, 54);
   interval -= delta;
   if (interval <= 0) {
-    const std::uniform_real_distribution<> rndInterval(0.1, 1.0);
+    const std::uniform_real_distribution<> rndInterval(0.1, 1.0 - static_cast<double>(stageNo) * 0.1);
     const std::uniform_int_distribution<> rndAddingCount(1, 5);
-    for (int i = rndAddingCount(game.Rand()); i > 0; --i) {
+    for (int i = rndAddingCount(game.Rand()) + (stageNo * 4) / 3; i > 0; --i) {
       const glm::vec3 pos(distributerX(game.Rand()), 0, distributerZ(game.Rand()));
       if (Entity::Entity* p = game.AddEntity(EntityGroupId_Enemy, pos, "Toroid", "Res/Model/Toroid.bmp", "Res/Model/Toroid.Normal.bmp", UpdateToroid())) {
         p->Velocity(glm::vec3(pos.x < 0 ? 3.0f : -3.0f, 0, -12.0f));
